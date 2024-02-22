11e65967fbe8ea7c5d4ffd63e255e6c0d8da3195
Christopher McClain <christopher.mcclain@icloud.com>
2024-02-22 15:09:50 -0700

    add landing text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="landingBlock">
+    <h2 class="landingTitle">Hello! I am Christopher, <br>a software engineer from Utah.</h2>
+    <p>I have researched alternatives to pasword based authentication, automated application deployments into the AWS cloud,
+        and written API's for User lifecycle management.</p>
+    <p>Now, I write code that protects users the moment they connect to the internet.</p>
+    <a href="mailto:christopher.mcclain@icloud.com">christopher.mcclain@icloud.com</a>
+</div>
+
+<style>
+    div {
+        margin-top: 30px;
+        margin: 0 20%;
+        padding: 0 30px;
+        box-sizing: border-box;
+        max-width: 600px;
+        padding-top: 50px;
+    }
+
+    p {
+        margin: 30px 0 30px 0;
+    }
+
+    a {
+        color: black;
+        transition: all 400ms ease-in-out;
+    }
+
+    a:hover {
+        color: white;
+        background-color: #EC994B;
+        border-radius: 4px;
+    }
+
+    @media(max-width: 800px) {
+        div {
+            margin: 0 10%;
+        }
+    }
+
+    @media(max-width: 400px) {
+        div {
+            margin: 0 5%;
+        }
+    }
+</style>
+
